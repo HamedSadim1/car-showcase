3594c136d88a93f82906482d7cd83941f5743861
HamedSadim1 <hamid.sadim@outlook.com>
2024-04-25 21:38:53 +0200

    update next js & fix missey key error

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default async function Home({ searchParams }: HomeProps) {
     model: searchParams.model || "",
   });
 
+
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
@@ -36,8 +37,8 @@ export default async function Home({ searchParams }: HomeProps) {
         {!isDataEmpty ? (
           <section>
             <div className="home__cars-wrapper">
-              {allCars?.map((car) => (
-                <CarCard car={car} />
+              {allCars?.map((car,i) => (
+                <CarCard key={i} car={car} />
               ))}
             </div>
 
